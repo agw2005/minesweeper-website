26ab11c1105d0059d0421f9c20e9aed2b53c38ce
Danial Al Ghazali Walangadi <danialagw@students.unnes.ac.id>
2024-08-06 23:24:49 +0700

    reset and added tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import Board from "./components/board";
-
 const numberOfTiles = 64;
 
 function App() {
-  return (
-    <>
-      <h1>Minesweeper</h1>
-      <Board n={numberOfTiles} />
-    </>
-  );
+  return <h2 className="text-red-300 font-extrabold">{numberOfTiles}</h2>;
 }
 
 export default App;
--- src/components/tile.tsx
+++
@@ -1,5 +0,0 @@
-function Tile() {
-  return <div id="tile">{1}</div>;
-}
-
-export default Tile;
